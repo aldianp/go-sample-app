56108e81eafe13d16185effdb07d283416cf09e7
yoga-putra_1701 <yoga.putra@2k.com>
2023-07-18 10:42:15 +0700

    test consume module

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/aldianp/go-sample-app
+
+go 1.20
+
+require github.com/aldianp/go-basic-helper v1.0.1 // indirect
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	helper_math "github.com/aldianp/go-basic-helper"
+)
+
+func main() {
+	fmt.Println("test")
+	i := helper_math.GetFactorial(5)
+	fmt.Println(i)
+}
